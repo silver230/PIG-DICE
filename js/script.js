c8295bd06e0871091b8f342e483d1326b25f0de3
sylvester omondi <sylvesteromondi49@gmail.com>
2018-09-28 17:05:35 +0300

    added math.random and math.floor function to round off the number passed as parameter to its nearest integer and return the number float

--- js/script.js
+++ js/script.js
@@ -7,5 +7,12 @@ var pigDice = {
   rollDice: function() {
     var roll =math.floor(math.random() *6) +1 //to round off the number passed as parameter to its nearest integer and return the number float
 
-  }
+     if(roll ===1){
+       this.currentScore =0;
+       this.switchActivePlayer();
+     } else {
+         this.currentScore += roll;
+     }
+       return roll;
+  }, // to show when the player to continue making their move s or switch
 }
